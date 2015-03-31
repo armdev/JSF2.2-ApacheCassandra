829a6c9950f9644f425fc928df790bbf5e1eafbe
Armen <armen.arzumanyan@gmail.com>
2015-03-31 17:49:12 +0400

    pushing jsf + cassandra, update

--- jsf-cassandra-master/src/main/java/com/project/web/beans/DatabaseBean.java
+++ jsf-cassandra-master/src/main/java/com/project/web/beans/DatabaseBean.java
@@ -10,10 +10,12 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.set;
 import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -30,12 +32,11 @@ public class DatabaseBean implements Serializable {
 
     private PreparedStatement selectAllUsers;
     private PreparedStatement insertUser;
-  
 
     @PostConstruct
     private void initDB() {
         System.out.println("Init database Bean!!!!!!!");
-       
+
         cluster = Cluster.builder()
                 .addContactPoint("localhost")
                 // .withSSL() // Uncomment if using client to node encryption
@@ -57,7 +58,7 @@ public class DatabaseBean implements Serializable {
                 + "id uuid ,"
                 + "firstname varchar, "
                 + "lastname varchar,"
-                + "email varchar,"               
+                + "email varchar,"
                 + "PRIMARY KEY (status, age, id, firstname, lastname, email)"
                 + ");");
 
@@ -67,32 +68,64 @@ public class DatabaseBean implements Serializable {
         );
     }
 
-    
     public void createPerson(User user) {
         session.execute(insertUser.bind(1, user.getAge(), user.getFirstname(), user.getLastname(), user.getEmail()));
+    }
+
+    public void deleteUser(String id) {
+        if (id != null) {
+            Statement statement = QueryBuilder.delete()
+                    .from("socnet", "user")
+                    .where(eq("id", UUID.fromString(id)));
+            session.execute(statement);
+        }
+    }
+
+    public void updateUser(User user) {
+        if (user.getId() != null) {
+            Statement statement = QueryBuilder.update("socnet", "user").with(set("firstname", user.getFirstname())).and(set("lastname", user.getLastname()))
+                    .and(set("email", user.getEmail()))
+                    .and(set("age", user.getAge()))
+                    .where(eq("id", UUID.fromString(user.getId().toString())));
+            session.execute(statement);
+        }
     }
 
     public List<User> getUserList() {
         List<User> userList = new ArrayList<User>();
         ResultSet results = session.execute(selectAllUsers.bind());
         for (Row row : results) {
-            userList.add(new User(row.getInt("status"),(row.getUUID("id")), row.getString("firstname"), row.getString("lastname"), row.getString("email"), row.getInt("age")));
-        }
-        return userList;
-    }
-    
-    
-     public List<User> getUserListSorted() {
-        Statement statement = QueryBuilder.select().from("socnet", "user").where(eq("status", 1)).orderBy(QueryBuilder.desc("age")).limit(1000000).allowFiltering();
-        List<User> userList = new ArrayList<User>();
-        ResultSet results = session.execute(statement);        
-        for (Row row : results) {
-            userList.add(new User(row.getInt("status"),(row.getUUID("id")), row.getString("firstname"), row.getString("lastname"), row.getString("email"), row.getInt("age")));
+            userList.add(new User(row.getInt("status"), (row.getUUID("id")), row.getString("firstname"), row.getString("lastname"), row.getString("email"), row.getInt("age")));
         }
         return userList;
     }
 
-   
+    public User getUserById(String id) {
+        Statement statement = QueryBuilder.select().from("socnet", "user").where(eq("id", UUID.fromString(id)));
+        User user = new User();
+        Row row = session.execute(statement).one();
+        if (row != null) {
+            user.setAge(row.getInt("age"));
+            user.setStatus(row.getInt("status"));
+            user.setFirstname(row.getString("firstname"));
+            user.setLastname(row.getString("lastname"));
+            user.setEmail(row.getString("email"));
+            user.setId(row.getUUID("id"));
+        }
+
+        return user;
+    }
+
+    public List<User> getUserListSorted() {
+        Statement statement = QueryBuilder.select().from("socnet", "user").where(eq("status", 1)).orderBy(QueryBuilder.desc("age")).limit(1000000).allowFiltering();
+        List<User> userList = new ArrayList<User>();
+        ResultSet results = session.execute(statement);
+        for (Row row : results) {
+            userList.add(new User(row.getInt("status"), (row.getUUID("id")), row.getString("firstname"), row.getString("lastname"), row.getString("email"), row.getInt("age")));
+        }
+        return userList;
+    }
+
     @PreDestroy
     private void stopDB() {
         cluster.close();
